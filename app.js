b33c5030e133b0f72cd5dba5c2f74b03a19e83cb
thvinicc <thbrazini@gmail.com>
2019-07-29 22:06:37 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAJ__mMbW_divMg2SOWJw5QtLcrYZ0wCXI",
+      authDomain: "usuarios-34cab.firebaseapp.com",
+      databaseURL: "https://usuarios-34cab.firebaseio.com",
+      projectId: "usuarios-34cab",
+      storageBucket: "usuarios-34cab.appspot.com",
+      messagingSenderId: "954173988358",
+      appId: "1:954173988358:web:413e7ba15503247c"
+    };
+    firebase.initializeApp(config);
